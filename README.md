b32cf5809589fa1c378aaeba408d7301408bca50
Luana Faria Lane <luanasfff@gmail.com>
2020-01-08 21:09:08 +0000

    Done.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+
+
+
+.
+
+
 # Refactoring with Modules
 
 ## Objectives
---
+++ lib/concerns/findable.rb
@@ -0,0 +1,17 @@
+module Findable
+
+  def find_by_name(name)
+    all.find {|a| a.name == name}
+  
+  end
+
+end
+
+
+ 
+ 
+ 
+ 
+ 
+  
+  
